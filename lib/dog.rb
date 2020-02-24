48f828336abc237c889544790272465d6c6bde9b
Elijah wines <elijahwines84@yahoo.com>
2020-02-24 13:56:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
 
-  def name
-    @this_dogs_name
-  end
+  
 end
 
-fido = Dog.new
-fido.name = "Fido"
-puts fido.name
